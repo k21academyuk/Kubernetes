dc4c6e4f28456581558c51082d0847856ef69747
k21academyuk <69501420+k21academyuk@users.noreply.github.com>
2024-05-09 11:16:21 +0530

    Update provider.tf

--- eks-terraform/provider.tf
+++ eks-terraform/provider.tf
@@ -1,4 +1,7 @@
+// Define the AWS provider block to specify AWS as the cloud provider for this Terraform configuration
 provider "aws" {
+   // Specify the AWS profile to be used for authentication
    profile    = "default"
+   // Specify the AWS region where the resources will be created
    region     = "eu-west-3"
  }
