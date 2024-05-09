46f20be7caf3c35a09c1f65da4ad0ef020b998da
k21academyuk <69501420+k21academyuk@users.noreply.github.com>
2024-05-09 11:15:09 +0530

    Update outputs.tf

--- eks-terraform/outputs.tf
+++ eks-terraform/outputs.tf
@@ -1,3 +1,5 @@
+// Define an output block to expose the name of the EKS cluster
 output "eks_cluster_name" {
+  // Set the value of the output to be the name of the EKS cluster
   value = aws_eks_cluster.aws_eks.name
 }
